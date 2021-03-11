3cc75ab1f59718999e7168cc6607a741a4359bd1
333 <333u@protonmail.com>
2021-03-11 19:37:52 +0100

    reformat

--- bulkmove.sh
+++ bulkmove.sh
@@ -7,18 +7,29 @@ escape="s/[\\ 	\"']/\\\\&/"
 movecmd='[ $0 = dry ] && alias mv="echo mv" rm="echo rm"
   if   [ "$2" = //  ];   then  rm "$1"
   elif [ "$1" != "$2" ]; then  mv "$1" "$2"; fi'
+
 err() { echo $1 >&2 ; exit 1; }
 
-while [ "$1" ]; do case "$1" in
+while [ "$1" ]; do
+    case "$1" in
         (-d) cmd='dry';;
         (-e) shift; editor="$1";;
         (-h) echo "usage: bulkmove [-d] [-e editor] [-f file ...]" >&2; exit;;
         (-f) alias ls='while shift; do echo "$1"; done'; break;;
     esac; shift
 done
-old=`mktemp` && new=`mktemp` || err 'temp file cannot be created'
+
+old=`mktemp` && new=`mktemp` ||
+    err 'temp file cannot be created'
+
 ls | tee $new | sed "$escape" > $old
+
 $editor $new
-[ `cat $old | wc -l` != `cat $new | wc -l` ] && err 'line count differs'
-cat $new | sed "$escape" | paste $old - | xargs -n 2 sh -c "$movecmd" "$cmd"
+
+[ `cat $old | wc -l` != `cat $new | wc -l` ] &&
+    err 'added or deleted lines'
+
+cat $new | sed "$escape" | paste $old - |
+    xargs -n 2 sh -c "$movecmd" "$cmd"
+
 rm $old $new
